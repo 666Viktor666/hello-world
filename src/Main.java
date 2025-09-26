46b1628689c79be2438be8d52c2d53abf3ebc45b
QA/auto <steputenkoviktor@gmail.com>
2025-09-26 10:59:48 +0300

    Initial commit: Hello World project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Мой первый проект через IDEA!");
+        System.out.println("Hello from IntelliJ IDEA!");
+
+        // Добавим простой пример
+        int number = 10;
+        String message = "Число: ";
+        System.out.println(message + number);
+    }
+}
