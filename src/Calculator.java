72d7921a5d7b6b5e9892218533a3a06021de5412
QA/auto <steputenkoviktor@gmail.com>
2025-09-26 14:38:30 +0300

    добавил калькулятор

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль!");
+        }
+        return (double) a / b;
+    }
+
+    public int power(int base, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,18 @@ public class Main {
         Name userName = new Name("Виктор", "Степутенко");
         System.out.println("Полное имя: " + userName.getFullName());
         System.out.println("Инициалы: " + userName.getInitials());
+
+        // Тестируем новый класс Calculator
+        System.out.println("\n=== Калькулятор ===");
+        Calculator calc = new Calculator();
+
+        int x = 20;
+        int y = 5;
+
+        System.out.println(x + " + " + y + " = " + calc.add(x, y));
+        System.out.println(x + " - " + y + " = " + calc.subtract(x, y));
+        System.out.println(x + " * " + y + " = " + calc.multiply(x, y));
+        System.out.println(x + " / " + y + " = " + calc.divide(x, y));
+        System.out.println(x + " ^ 2 = " + calc.power(x, 2));
     }
 }
