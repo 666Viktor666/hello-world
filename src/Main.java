1aa49e77e08e6dd07f90a704235d89ef63d41dee
QA/auto <steputenkoviktor@gmail.com>
2025-09-26 11:10:07 +0300

    Hello World project New

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,17 @@ public class Main {
         System.out.println("Мой первый проект через IDEA!");
         System.out.println("Hello from IntelliJ IDEA!");
 
-        // Добавим простой пример
+        // Добавим новый функционал
         int number = 10;
         String message = "Число: ";
         System.out.println(message + number);
+
+        // НОВЫЙ КОД - вычисляем квадрат числа
+        int square = number * number;
+        System.out.println("Квадрат числа: " + square);
+
+        // Ещё новое - приветствие по имени
+        String name = "Виктор";
+        System.out.println("Привет, " + name + "!");
     }
 }
